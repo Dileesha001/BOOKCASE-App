71befd820f4ccea44d33b172b5bf471f1648519d
Dileesha Ravishan <dilravishan321@gmail.com>
2026-07-24 00:20:29 +0530

    Fix StockBadge import from react-native

--- src/components/StockBadge.js
+++ src/components/StockBadge.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react'
+import { View, Text, StyleSheet } from 'react-native';
 import { useTheme } from '../context/ThemeContext';
 
 export const StockBadge = ({ status }) => {
